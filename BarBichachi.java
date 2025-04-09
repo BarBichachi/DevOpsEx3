d9a0370036fbaccd40b693832ecdf72ff43133f4
Bar Bichachi <barbi2@mta.ac.il>
2025-04-09 19:13:59 +0300

    Update BarBichachi.java with latest changes

--- BarBichachi.java
+++ BarBichachi.java
@@ -1,11 +1,7 @@
 public class BarBichachi { 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		for (int i=1; i<=5; i++) {
-=======
 		for (int i=1; i<=10; i++) {
->>>>>>> 6d3e442c6a89591fffa5fefd22b9859c4545f35e
-			System.out.println("Bar Bichachi");
+			System.out.println("A new print");
 		}
 	}
 }
