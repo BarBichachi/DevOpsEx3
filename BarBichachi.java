1beb9f34b9c00e8c5e010c9e71a88f8c2ed512d1
Bar Bichachi <barbi2@mta.ac.il>
2025-04-09 19:04:40 +0300

    Added Hello and BarBichachi

---
+++ BarBichachi.java
@@ -0,0 +1,11 @@
+public class BarBichachi { 
+	public static void main(String[] args) {
+<<<<<<< HEAD
+		for (int i=1; i<=5; i++) {
+=======
+		for (int i=1; i<=10; i++) {
+>>>>>>> 6d3e442c6a89591fffa5fefd22b9859c4545f35e
+			System.out.println("Bar Bichachi");
+		}
+	}
+}
